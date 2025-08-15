cb3dae0e5ddca6c2028ee498a4753bed37017e62
sahil-salunke <sahil.salunke6194@gmail.com>
2025-08-15 23:31:46 +0530

    Added lombok

--- src/main/java/org/practice/entity/EmployeeDT.java
+++ src/main/java/org/practice/entity/EmployeeDT.java
@@ -2,36 +2,16 @@ package org.practice.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Entity
+@Slf4j
+@Data
 public class EmployeeDT {
 
     @Id
     private long emp_id;
     private String emp_name;
     private String designation;
-
-    public long getEmp_id() {
-        return emp_id;
-    }
-
-    public void setEmp_id(long emp_id) {
-        this.emp_id = emp_id;
-    }
-
-    public String getEmp_name() {
-        return emp_name;
-    }
-
-    public void setEmp_name(String emp_name) {
-        this.emp_name = emp_name;
-    }
-
-    public String getDesignation() {
-        return designation;
-    }
-
-    public void setDesignation(String designation) {
-        this.designation = designation;
-    }
 }
